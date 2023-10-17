115b7422e0aba9f809430249ae98fb008e880df3
shri0900 <144302909+shri0900@users.noreply.github.com>
2023-10-17 21:47:50 +0530

    added functionality

--- indexscript.js
+++ indexscript.js
@@ -1,53 +1,60 @@
 let accessToken = "";
-        let instanceUrl = "";
+    let instanceUrl = "";
 
-        function authenticate() {
-            // Salesforce OAuth2 endpoint
-            const url = "https://login.salesforce.com/services/oauth2/authorize?" +
-                "response_type=token&" +
-                "client_id=3MVG9wt4IL4O5wvIrDAEJFQDJvCD2CuxIBViCEsFByBvbpFQgRN1szQBNOS6T9Km0sDco92ms7crHktOaf5yZ&" +
-                "redirect_uri=https://shri0900.github.io/BookYourShowApp/";
+    function authenticate() {
+        // Salesforce OAuth2 endpoint
+        const url = "https://login.salesforce.com/services/oauth2/authorize?" +
+            "response_type=token&" +
+            "client_id=3MVG9wt4IL4O5wvIrDAEJFQDJvCD2CuxIBViCEsFByBvbpFQgRN1szQBNOS6T9Km0sDco92ms7crHktOaf5yZ&" +
+            "redirect_uri=https://shri0900.github.io/BookYourShowApp/";
 
-            window.location.href = url;
-        }
+        window.location.href = url;
+    }
 
-        function parseReturnedHash() {
-            const hash = window.location.hash.substring(1);
-            const params = new URLSearchParams(hash);
-            accessToken = params.get("access_token");
-            instanceUrl = params.get("instance_url");
-        }
+    function parseReturnedHash() {
+        const hash = window.location.hash.substring(1);
+        const params = new URLSearchParams(hash);
+        accessToken = params.get("access_token");
+        instanceUrl = params.get("instance_url");
+    }
 
-        window.onload = () => {
-            parseReturnedHash();
-        }
+    window.onload = () => {
+        parseReturnedHash();
+    }
 
-        document.getElementById('getConcert').addEventListener('click', function(event) {
-            event.preventDefault();
-            const token = accessToken; // Replace with your actual token
-            const instanceURL = instanceUrl; 
-            const concertName = document.getElementById('concertName').value; 
-            
-            fetch(`${instanceURL}/services/apexrest/getConcerts/${concertName}`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log("Data"+JSON.stringify(data))
-
-                if (data) {
-                    document.getElementById("modalbox").style.display = "block"; // Show element
-                } else {
-                    document.getElementById("modalbox").style.display = "none"; // Hide element
-                }
-                
-               
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+    document.getElementById('getConcert').addEventListener('click', function(event) {
+        event.preventDefault();
+        const token = accessToken; // Replace with your actual token
+        const instanceURL = instanceUrl; 
+        const concertName = document.getElementById('concertName').value; 
+        
+        fetch(`${instanceURL}/services/apexrest/getConcerts/${concertName}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Data"+JSON.stringify(data))
+            //if data is available show this modal box
+            if (data && data.length > 0) {
+                const concert = data[0];
+                document.getElementById('concertName').textContent = concert.Name;
+                document.getElementById('concertType').textContent = `Type: ${concert.Concert_Type__c}`;
+                document.getElementById('concertVenue').textContent = `Venue: ${concert.Concert_Venue__c}`;
+                document.getElementById("modalbox").style.display = "block"; // Show element
+            } else {
+                document.getElementById("modalbox").style.display = "none"; // Hide element
+            }
+            //modal boc close functionality
+            const closeButton = document.getElementById('close-button');
+            closeButton.onclick = function() {
+                modal.style.display = "none";
+            } 
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
+    });
