d465f7e16785e70ca74fe3d59ae909691be79a3b
shri0900 <144302909+shri0900@users.noreply.github.com>
2023-10-27 09:29:33 +0530

    button UI changes

--- indexscript.js
+++ indexscript.js
@@ -250,12 +250,12 @@ document.addEventListener("DOMContentLoaded", function() {
                                     <p class="card-text">Price: (₹) ${item.Price__c}</p>
                                     <img src="${item.Concert_Promotion_Image_Url__c}" alt="Taylor Swift" style="height: 200px; width: 100%; object-fit: cover;">
                                     <br>
-                                    button class="btn btn-secondary open-modal"
-                    data-id="${item.Id}"
-                    data-name="${item.Name}"
-                    data-date="${item.Date_of_Concert__c}">
-                Book Now!
-            </button>
+                                    <button class="btn btn-secondary open-modal"
+                                                  data-id="${item.Id}"
+                                                  data-name="${item.Name}"
+                                                  data-date="${item.Date_of_Concert__c}">
+                                                     Book Now!
+                                  </button>
                                 </div>
                             </div>
                         </div>
