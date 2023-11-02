745e31806834148908f7346137f8594718551698
shri0900 <144302909+shri0900@users.noreply.github.com>
2023-11-02 13:06:32 +0530

    reviews js page changed

--- reviews.js
+++ reviews.js
@@ -1,28 +1,46 @@
-function retrieveInstanceAndAccessToken() {
-    // Retrieve the instanceUrl and accessToken from Local Storage
-    let instanceUrl = localStorage.getItem('instanceUrl');
-    let accessToken = localStorage.getItem('accessToken');
-  
-    // Check if they exist and are not null
-    if (instanceUrl && accessToken) {
-      // You can use the instanceUrl and accessToken for making API calls
-      console.log("Instance URL from Local Storage: " + instanceUrl);
-      console.log("Access Token from Local Storage: " + accessToken);
-    } else {
-      // Handle the case where they are not found in Local Storage
-      // You might want to redirect the user to the login page or perform some other action.
-      console.log("Instance URL and Access Token not found in Local Storage.");
-    }
-  }
+// Function to make an API call to Salesforce and display the reviews on the UI
+function fetchAndDisplayReviews(instanceUrl, accessToken) {
+  // Construct the REST API URL for your Salesforce RESTful method
+  const apiUrl = `${instanceUrl}/services/apexrest/getReviews`;
 
-  // Add an event listener to the "Review" page link
-document.getElementById('reviewLink').addEventListener('click', function (event) {
-    // Prevent the default link behavior
-    event.preventDefault();
-  
-    // Call the function to retrieve instanceUrl and accessToken
-    retrieveInstanceAndAccessToken();
-  
-    // You can also add code to load the "Review" page content here.
+  // Make an HTTP GET request to the Salesforce REST API
+  fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+      }
+  })
+  .then(response => response.json())
+  .then(data => {
+      // Handle the data received from the API call
+      // You can update your UI to display the reviews here
+      console.log("Reviews Data:", data);
+      // Example: Update your UI to display the reviews data
+      // document.getElementById('reviewsContainer').innerHTML = JSON.stringify(data, null, 2);
+  })
+  .catch(error => {
+      console.error('Error fetching reviews:', error);
   });
+}
+
+// Add an event listener to the "Review" page link
+document.getElementById('reviewLink').addEventListener('click', function (event) {
+  // Prevent the default link behavior
+  event.preventDefault();
+
+  // Retrieve the instanceUrl and accessToken from Local Storage
+  let instanceUrl = localStorage.getItem('instanceUrl');
+  let accessToken = localStorage.getItem('accessToken');
+
+  // Check if they exist and are not null
+  if (instanceUrl && accessToken) {
+      // Call the function to fetch and display reviews
+      fetchAndDisplayReviews(instanceUrl, accessToken);
+  } else {
+      // Handle the case where instanceUrl and accessToken are not found
+      console.log("Instance URL and Access Token not found in Local Storage.");
+  }
+});
+
   
