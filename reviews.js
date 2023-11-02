e2888c0d94ec3f802be1b59c9ae5a5a45d8d5264
shri0900 <144302909+shri0900@users.noreply.github.com>
2023-11-02 13:50:58 +0530

    data added to scroospy

--- reviews.js
+++ reviews.js
@@ -16,8 +16,36 @@ function fetchAndDisplayReviews(instanceUrl, accessToken) {
       // Handle the data received from the API call
       // You can update your UI to display the reviews here
       console.log("Reviews Data:", JSON.stringify(data));
-      // Example: Update your UI to display the reviews data
-      // document.getElementById('reviewsContainer').innerHTML = JSON.stringify(data, null, 2);
+
+
+      // Reference to the Scrollspy container
+  const scrollspyContainer = document.querySelector('.scrollspy-example');
+  
+  // Iterate through the reviews data and create the content
+  reviewsData.forEach((review, index) => {
+    // // Create an anchor link for the navigation menu
+    const navLink = document.createElement('a');
+    navLink.className = 'nav-link';
+    navLink.href = `#scrollspyHeading${index + 1}`;
+   // navLink.textContent = `Review ${index + 1}`;
+  
+    // Create a Scrollspy heading for each review
+    const heading = document.createElement('h4');
+    heading.id = `scrollspyHeading${index + 1}`;
+    heading.textContent = review.Name;
+  
+    // Create a paragraph for the detailed review content
+    const reviewContent = document.createElement('p');
+    reviewContent.innerHTML = review.Detailed_Review__c;
+  
+    // Append the elements to the Scrollspy container
+    scrollspyContainer.appendChild(heading);
+    scrollspyContainer.appendChild(reviewContent);
+  
+    // Append the navigation link to the navigation menu
+    document.querySelector('.nav').appendChild(navLink);
+  });
+      
   })
   .catch(error => {
       console.error('Error fetching reviews:', error);
