b0e60044aec3bf05b6135e339db28963b2863a8b
shri0900 <144302909+shri0900@users.noreply.github.com>
2023-11-15 23:02:27 +0530

    bug fixing

--- bookings.js
+++ bookings.js
@@ -134,7 +134,7 @@ function updateOffcanvasContentLeft(data) {
     list.appendChild(listItem);
   });
 
-  const offcanvasBody = document.querySelector('.offcanvas-body left"');
+  const offcanvasBody = document.querySelector('.offcanvas-body left');
   offcanvasBody.innerHTML = ''; // Clear previous content
   offcanvasBody.appendChild(list);
 
