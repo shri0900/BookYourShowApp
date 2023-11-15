b14cde4616f157c2973f48dada4f9daadff432cd
shri0900 <144302909+shri0900@users.noreply.github.com>
2023-11-15 18:19:43 +0530

    offcanvas added

--- bookings.js
+++ bookings.js
@@ -1,64 +1,3 @@
-// function retrieveInstanceAndAccessToken() {
-//     // Retrieve the instanceUrl and accessToken from Local Storage
-//     let instanceUrl = localStorage.getItem('instanceUrl');
-//     let accessToken = localStorage.getItem('accessToken');
-  
-//     // Check if they exist and are not null
-//     if (instanceUrl && accessToken) {
-//       // You can use the instanceUrl and accessToken for making API calls
-//       console.log("Instance URL from Local Storage: " + instanceUrl);
-//       console.log("Access Token from Local Storage: " + accessToken);
-//     } else {
-//       // Handle the case where they are not found in Local Storage
-//       // You might want to redirect the user to the login page or perform some other action.
-//       console.log("Instance URL and Access Token not found in Local Storage.");
-//     }
-//   }
-
-//   // Add an event listener to the "Review" page link
-// document.getElementById('bookingLink').addEventListener('click', function (event) {
-//     // Prevent the default link behavior
-//     event.preventDefault();
-  
-//     // Call the function to retrieve instanceUrl and accessToken
-//     retrieveInstanceAndAccessToken();
-//     populateIndividuals(instanceUrl, accessToken);
-//     // You can also add code to load the "Review" page content here.
-//   });
-  
-
-//   function populateIndividuals(instanceUrl, accessToken) {
-//     const apiUrl = `${instanceUrl}/services/apexrest/getIndividuals`;
-  
-//     fetch(apiUrl, {
-//       method: 'GET',
-//       headers: {
-//         'Authorization': `Bearer ${accessToken}`,
-//         'Content-Type': 'application/json'
-//       }
-//     })
-//       .then(response => response.json())
-//       .then(data => {
-//         console.log("Individual Options  Data:", JSON.stringify(data));
-//         const individualSelect = document.getElementById('individualSelect');
-  
-//         data.forEach(individual => {
-//           const option = document.createElement('option');
-//           option.value = individual.Id;
-//           option.textContent = individual.Name;
-//           individualSelect.appendChild(option);
-//         });
-//       })
-//       .catch(error => {
-//         console.error('Error fetching individuals:', error);
-//       });
-//   }
-  
-//   // Add an event listener to the select element
-//   document.getElementById('individualSelect').addEventListener('change', function () {
-//     const selectedValue = this.value;
-//     document.getElementById('selectedIndividualId').value = selectedValue;
-//   });
 
 let instanceUrl; // Declare these variables in a scope accessible to other functions
 let accessToken;
@@ -124,4 +63,10 @@ function populateIndividuals(instanceUrl, accessToken) {
 document.getElementById('individualSelect').addEventListener('change', function () {
   const selectedValue = this.value;
   document.getElementById('selectedIndividualId').value = selectedValue;
+});
+
+// Use Bootstrap v5 syntax to trigger offcanvas
+var historyOffcanvas = new bootstrap.Offcanvas(document.getElementById('historyOffcanvas'));
+document.getElementById('showHistoryButton').addEventListener('click', function () {
+   historyOffcanvas.toggle();
 });
