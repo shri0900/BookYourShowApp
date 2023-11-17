2dbb47d34b9e21b8536b728bdf187f967802f609
shri0900 <144302909+shri0900@users.noreply.github.com>
2023-11-17 10:44:11 +0530

    UI Changes

--- concerts.js
+++ concerts.js
@@ -67,13 +67,14 @@ function renderUpcomingShows(upcomingShows) {
   upcomingShows.forEach(show => {
     // Create a new card element with Bootstrap classes
     let card = document.createElement('div');
-    card.className = 'card mb-3';
-    card.style = 'max-width: 540px;';
+    card.className = 'card mb-3 my-3';
+    card.style = 'max-width: 340px;';
+    card.style='max-height: 340px;';
 
     // Create an image element for the card with Bootstrap class
     let img = document.createElement('img');
     img.src = show.Show_Promotion_Image_URL__c; // Assuming Promotional_Picture__c contains the image URL
-    img.className = 'card-img-top';
+    img.className = 'card-img-centre';
     img.alt = show.Name;
 
     // Create a card body element with Bootstrap class
