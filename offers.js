d894b82a1fe132a10075187c38bddefdfd8fb4d8
shri0900 <144302909+shri0900@users.noreply.github.com>
2023-12-06 19:53:54 +0530

    changes

--- offers.js
+++ offers.js
@@ -19,7 +19,7 @@ function retrieveInstanceAndAccessToken() {
     // Check if they exist and are not null
     if (instanceUrl && accessToken) {
       // Make an API call to get offers data
-      fetch(instanceUrl + '/services/data/v59.0/query/?q=SELECT+Id,Name,Discount_Percentage__c+FROM+Offer__c', {
+      fetch(instanceUrl + '/services/data/services/apexrest/getOffers', {
         method: 'GET',
         headers: {
           'Authorization': 'Bearer ' + accessToken
